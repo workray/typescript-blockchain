0c9c5db433d8a6674311dd220d4d53e31aefb5cc
Raymond <workrayz@gmail.com>
2018-05-30 09:12:43 -0400

    Types in Typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Raymond", age = 32, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(name, age, gender);
-//# sourceMappingURL=index.js.map
