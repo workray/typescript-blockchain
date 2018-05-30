1dcdb8f3d3901b5f94077676519139fcd55ee90e
Raymond <workrayz@gmail.com>
2018-05-30 10:20:35 -0400

    final

--- src/index.ts
+++ src/index.ts
@@ -41,13 +41,14 @@ const createNewBlock = (data: string) : Block => {
   const newTimestamp: number = getNewTimeStamp();
   const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
   const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+  addBlock(newBlock);
   return newBlock;
 }
 
 const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
 
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
-  if(Block.validateStructure(candidateBlock)) {
+  if(!Block.validateStructure(candidateBlock)) {
     return false;
   } else if (previousBlock.index + 1 !== candidateBlock.index) {
     return false;
@@ -65,5 +66,11 @@ const addBlock = (candidateBlock: Block) : void => {
     blockchain.push(candidateBlock);
   }
 };
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
